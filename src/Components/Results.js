994f1cd18b1de3e56fc46b7f54f871d994cb3223
Birte Müller <birte.mueller@outlook.com>
2018-09-04 21:33:33 +0200

    add default query

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -23,7 +23,7 @@ export default class Results extends Component  {
         this.performSearch();
     }
 
-    performSearch = (query) => {
+    performSearch = (query = 'farming') => {
         axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=20&format=json&nojsoncallback=1`)
             .then(response => {
                 this.setState({
