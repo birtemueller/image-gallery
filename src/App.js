7b6eb345421e8416e42cf9343c166a3403b466aa
Birte Müller <birte.mueller@outlook.com>
2018-10-04 21:51:35 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -4,26 +4,31 @@ import {
     Route,
     Switch
 } from 'react-router-dom';
-import axios from 'axios';
 import './App.css';
 
-import SearchForm from './Components/SearchForm';
-import Navigation from './Components/Navigation';
 import Results from './Components/Results';
+import DeadEnd from './Components/404';
+import SearchForm from "./Components/SearchForm";
+import Navigation from "./Components/Navigation";
 
 export default class App extends Component {
     render() {
-
         return (
 
-            <div className="App">
+            <BrowserRouter forceRefresh={true}>
+                {/*forces the site to refresh after a new topic has been selected via navigation or search*/}
                 <div className="container">
-                    <SearchForm onSearch={this.performSearch}/>
-                    <Navigation/>
-                    <Results />
+                    <Navigation />
+                    <Switch>
+                        <Route path="/search/:query" render={ props => <Results query={ props.match.params.query }/> } />
+                        <Route exact path="/" render={ () => <Results query='Birds'/> } />
+                        {/*Search queries are passed to the Results component through the URL. The Default query when entering the page is 'Birds'.*/}
+                        <Route path="/search" component={SearchForm} />
+                        <Route component={DeadEnd} />
+                        {/*404 error route that displays when a URL path does not match a route. */}
+                    </Switch>
                 </div>
-            </div>
-
+            </BrowserRouter>
         );
     }
 }
