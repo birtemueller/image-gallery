b7097ffcd63b4d69a15f5cbf932886a0812adc0e
Birte Müller <birte.mueller@outlook.com>
2018-10-04 21:51:08 +0200

    add comments pass search query down add NoResults component

--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react';
-import {
-    BrowserRouter,
-    Route,
-    Switch
-} from 'react-router-dom';
 import axios from 'axios';
 import apiKey from '../Config'
 
 import Images from './Images';
+import NoResults from "./NoResults";
 
 export default class Results extends Component  {
 
@@ -23,13 +19,13 @@ export default class Results extends Component  {
         this.performSearch();
     }
 
-    performSearch = (query = 'farming') => {
-        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=20&format=json&nojsoncallback=1`)
+    performSearch = () => {
+        // fetches data from the flickr API and passes it to the state
+        axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${this.props.query}&per_page=20&format=json&nojsoncallback=1`)
             .then(response => {
                 this.setState({
                     images: response.data.photos.photo,
                     loading: false
-
                 })
             })
             .catch(error => {
@@ -41,16 +37,19 @@ export default class Results extends Component  {
     };
 
     render() {
-        console.log(this.state.images);
-        console.log(this.state.loading);
-        console.log(this.state.images.length);
-        console.log(this.props.query);
-
+        //indicates if search is still in progress
         if (this.state.loading)
             return <p>Loading...</p>;
 
+            //displays the NoResults component if no images have been found
             else {
-                return (<Images data={this.state.images} />)
+            if(this.state.images.length === 0){
+                return(
+                    <NoResults/>
+                )
+            }
+            //displays the images component including the data fetched from the flickr API
+            return (<Images data={this.state.images} query={this.props.query}/>)
         }
     }
 }
