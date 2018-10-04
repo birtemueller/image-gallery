edb8c85b70bdedd0654dc3f9ecd86e1d8a91ab8a
Birte Müller <birte.mueller@outlook.com>
2018-10-04 21:50:29 +0200

    add comments

--- src/Components/Image.js
+++ src/Components/Image.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//creates the list items for the image list.
+//properties are received from the 'Images' component
+
 const Image = props => (
     <li>
         <img src={props.url} alt={props.alt}/>
