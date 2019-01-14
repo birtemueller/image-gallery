a691067da219bfcdcd8f8871a0668a348a06fa99
Birte Müller <birte.mueller@outlook.com>
2019-01-14 12:47:37 +0100

    remove links from text, add comments

--- src/Components/404.js
+++ src/Components/404.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import {
-    NavLink
-} from 'react-router-dom';
 
 const DeadEnd = () => (
     <li className="not-found">
         <h3>Oooops!</h3>
-        <p>This page does not exist. Try the <NavLink to="/search">Search Field</NavLink> to continue.</p>
+        <p>This page does not exist. Try the Search Field to continue.</p>
     </li>
 );
 
--- src/Components/NoResults.js
+++ src/Components/NoResults.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
 
+/*404 page to be displayed in case the user has entered a non-valid URL.*/
 const NoResults = () => (
     <li className="not-found">
         <h3>No Results Found</h3>
-        <p>Your search did not return any results. <NavLink to="/search">Please try again.</NavLink></p>
+        <p>Your search did not return any results. Please try again.</p>
     </li>
 );
 
