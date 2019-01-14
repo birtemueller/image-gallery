479916aa9c7b9223bd4436f039790689793b2f9e
Birte Müller <birte.mueller@outlook.com>
2019-01-14 12:46:39 +0100

    Display the search form not only when in "search" mode Add "EmptySearch" component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Results from './Components/Results';
 import DeadEnd from './Components/404';
 import SearchForm from "./Components/SearchForm";
 import Navigation from "./Components/Navigation";
+import EmptySearch from "./Components/EmptySearch";
 
 export default class App extends Component {
     render() {
@@ -18,12 +19,14 @@ export default class App extends Component {
             <BrowserRouter forceRefresh={true}>
                 {/*forces the site to refresh after a new topic has been selected via navigation or search*/}
                 <div className="container">
+                    <SearchForm/>
                     <Navigation />
                     <Switch>
                         <Route path="/search/:query" render={ props => <Results query={ props.match.params.query }/> } />
-                        <Route exact path="/" render={ () => <Results query='Birds'/> } />
                         {/*Search queries are passed to the Results component through the URL. The Default query when entering the page is 'Birds'.*/}
-                        <Route path="/search" component={SearchForm} />
+                        <Route exact path="/" render={ () => <Results query='Birds'/> } />
+
+                        <Route path="/search"  component={EmptySearch} />
                         <Route component={DeadEnd} />
                         {/*404 error route that displays when a URL path does not match a route. */}
                     </Switch>
